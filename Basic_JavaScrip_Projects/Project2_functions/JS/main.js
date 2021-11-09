7bae4a60b9074d251392405091586e4c311f0daf
Cesarintapia <93293727+Cesarintapia@users.noreply.github.com>
2021-11-08 16:47:55 -0800

    update

---
+++ Basic_JavaScrip_Projects/Project2_functions/JS/main.js
@@ -0,0 +1,21 @@
+
+
+function My_Second_Funtion() {                                                           //initializing function
+    var A = "Salve, The King Loro!";                                               //defining variable an giving value
+    var B = A.fontcolor ("red");                                                 //using font color
+    document.getElementById ("Red_Text") .innerHTML=B;                         //putting value of result into HTML element with "Green_Text" id
+}
+
+
+function My_Funtion_Date(){
+    document.getElementById("date") .innerHTML=Date(); 
+}
+
+
+function myFunction() {                                                        //initializing function
+    var sentence = "I am Learning";                                           //defining variable string an giving text 
+    sentence += "   a lot from this book!!";                                 //defining the concatenation and adding text
+    document.getElementById("concatenate").innerHTML=sentence;              //performs the operation
+}
+
+
