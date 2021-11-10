663ff2d7b3dc8487a6595227468c1c6aec74767a
Cesarintapia <93293727+Cesarintapia@users.noreply.github.com>
2021-11-10 15:04:30 -0800

    coments were adding

--- Basic_JavaScrip_Projects/Project9_countdown_slideshow/JS/main.js
+++ Basic_JavaScrip_Projects/Project9_countdown_slideshow/JS/main.js
@@ -1,19 +1,19 @@
-var slideIndex = 1;
+var slideIndex = 1;                                              //setting general variable
 showSlides(slideIndex);
 
-function plusSlides(n) {
+function plusSlides(n) {                                         //starting function slide1
   showSlides(slideIndex += n);
 }
 
-function currentSlide(n) {
+function currentSlide(n) {                                        //starting function slide2
   showSlides(slideIndex = n);
 }
 
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
+function showSlides(n) {                                         //starting function
+  var i;                                                         //setting local variables
+  var slides = document.getElementsByClassName("mySlides");      
   var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
+  if (n > slides.length) {slideIndex = 1}                        //starting if function
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";  
@@ -21,6 +21,6 @@ function showSlides(n) {
   for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";  
+  slides[slideIndex-1].style.display = "block";                    //performing results
   dots[slideIndex-1].className += " active";
 }
